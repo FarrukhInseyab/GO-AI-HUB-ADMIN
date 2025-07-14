17dbdd51625c0b89c6bd238c3613c8f8a1e477b5
FarrukhInseyab <108561700+FarrukhInseyab@users.noreply.github.com>
2025-07-14 12:16:20 +0500

    Fix AuthContext Export for Fast Refresh

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,7 +3,7 @@ import { User } from '../types';
 import { supabase } from '../lib/supabase';
 import emailService from '../utils/emailService';
 
-type AuthContextType = {
+interface AuthContextType {
   user: User | null;
   isLoading: boolean;
   login: (email: string, password: string) => Promise<void>;
@@ -14,15 +14,15 @@ type AuthContextType = {
   resetPassword: (password: string) => Promise<void>;
 };
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
-export const useAuth = () => {
+export function useAuth() {
   const context = useContext(AuthContext);
-  if (context === undefined) {
+  if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
-};
+}
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
